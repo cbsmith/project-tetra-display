03e7ec93fd6f4c0f97628b9ffb835718ee92ede0
Fletcher Porter <me@fletcherporter.com>
2020-08-03 17:13:45 -0700

    added a check to the runtests.sh script to make sure the docker daemon is running and to notify the user if it's not.  Closes #8

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+HIGHLIGHT='\033[0;33m'
+NO_COLOR='\033[0m'
+
+docker ps >/dev/null 2>&1 || {
+   printf "${HIGHLIGHT}An error was encountered in running the behave tests.\nThe docker daemon is not running.${NO_COLOR}"
+   exit 1
+}
+
+
 echo "Behaviour Tests:"
 behave
 echo $'\nUnit Tests:'
